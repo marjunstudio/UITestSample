a27fdccf15960b71ac98b61dc51781712783acbf
Junnosuke Takahashi <uebujie@gmail.com>
2026-04-19 07:13:07 +0900

    Gifでキャプチャする設定を追加

--- composeApp/src/androidUnitTest/kotlin/com/app/uitestsample/RoborazziTest.kt
+++ composeApp/src/androidUnitTest/kotlin/com/app/uitestsample/RoborazziTest.kt
@@ -1,8 +1,12 @@
 package com.app.uitestsample
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.RoborazziRule
+import com.github.takahirom.roborazzi.captureRoboGif
 import com.github.takahirom.roborazzi.captureRoboImage
 import org.junit.Rule
 import org.junit.Test
@@ -18,11 +22,35 @@ class RoborazziTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    // Roborazziの共通ルール定義
+    @get:Rule
+    val roborazziRule = RoborazziRule(
+        composeRule = composeTestRule,
+        captureRoot = composeTestRule.onRoot(),
+        options = RoborazziRule.Options(
+            // 出力先を screenshots フォルダに一括指定
+            outputDirectoryPath = "screenshots"
+        )
+    )
+
     @Test
     fun captureAppScreenshot() {
         composeTestRule.setContent {
             App()
         }
         composeTestRule.onRoot().captureRoboImage()
+    }
+
+    @Test
+    fun captureAppAnimationGif() {
+        composeTestRule.setContent {
+            App()
+        }
+
+        composeTestRule.onRoot().captureRoboGif(
+            composeRule = composeTestRule,
+        ) {
+            composeTestRule.onNodeWithText("Click me!").performClick()
+        }
     }
 }
